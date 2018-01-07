5634fe2afbf24119cf00bc8a2561ee9171cac6ca
Seraphyx <jasonpark08@gmail.com>
2018-01-06 22:07:38 -0800

    Flask hello world

--- server/app.py
+++ server/app.py
@@ -3,3 +3,9 @@ import numpy as np
 import tensorflow as tf
 from functools import wraps
 from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
