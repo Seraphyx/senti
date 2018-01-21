cdbb7134bb31b107dc16682d1d812c429565407e
Seraphyx <seraphyxprier@gmail.com>
2018-01-21 15:17:58 -0800

    Working HAN model

--- model/train_han.py
+++ model/train_han.py
@@ -1,10 +1,16 @@
 import sys
 import os
 
-from keras_text.processing import WordTokenizer
+import numpy as np
+
+from keras.utils import to_categorical
+from keras.models import load_model
+
+from keras_text.processing import WordTokenizer, SentenceWordTokenizer
 from keras_text.data import Dataset
 from keras_text.models.token_model import TokenModelFactory
-from keras_text.models import YoonKimCNN, AttentionRNN, StackedRNN
+from keras_text.models import SentenceModelFactory
+from keras_text.models import YoonKimCNN, AttentionRNN, StackedRNN, AveragingEncoder
 from keras_text.utils import dump, load
 
 
@@ -25,23 +31,45 @@ See: https://raghakot.github.io/keras-text/
 # tokenizer.build_vocab(texts)
 
 
+def doc_to_sentence(doc_token_list, max_sents, max_tokens):
+
+	# Count how many sentences
+	n_doc = len(doc_token_list)
+
+	# Initialize
+	ds_embedding = np.zeros((n_doc, max_sents, max_tokens))
+
+	# Convert words to vector embeddings
+	for i, doc in enumerate(doc_token_list):
+		if i + 1 > n_doc:
+			break
+		for j, sentence in enumerate(doc):
+			if j + 1 > max_sents:
+				break
+			for k, word in enumerate(sentence):
+				if k + 1 > max_tokens:
+					break
+				ds_embedding[i, j, k] = word
+
+	return ds_embedding
+
 
 def save_folder(folder_path):
 	'''
 	Create folder and subfolders to save results
 	'''
 	if not os.path.exists(folder_path):
-    	os.makedirs(folder_path)
-    	os.makedirs(folder_path + '/models')
-    	os.makedirs(folder_path + '/embeddings')
-    	os.makedirs(folder_path + '/data')
-    	os.makedirs(folder_path + '/diagnostics')
-    	os.makedirs(folder_path + '/results')
+		os.makedirs(folder_path)
+		os.makedirs(folder_path + '/models')
+		os.makedirs(folder_path + '/embeddings')
+		os.makedirs(folder_path + '/dataset')
+		os.makedirs(folder_path + '/diagnostics')
+		os.makedirs(folder_path + '/results')
 
 
 def build_tokenizer(data):
 	print("=== Building Tokenizer")
-	tokenizer = WordTokenizer()
+	tokenizer = SentenceWordTokenizer()
 	tokenizer.build_vocab(data.data['x'])
 	return tokenizer
 
@@ -81,13 +109,18 @@ if __name__ == '__main__':
 	# Save all results into a directory
 	save_directory = '../data/train_han'
 	save_folder(save_directory)
+	model_name = 'HAN_1'
 
 
 	# Steps to perform
 	BUILD_TOKENIZER = False
 	BUILD_DATASET   = False
-	TRAIN_MODEL     = False
+	TRAIN_MODEL     = True
 	INFERENCE       = True
+
+	NUM_CLASSES = 2
+	MAX_SENTS = 100
+	MAX_TOKENS = 100
 
 	# Read data
 	print("=== Loading Data")
@@ -96,6 +129,7 @@ if __name__ == '__main__':
 	# Build a token. Be default it uses 'en' model from SpaCy
 	if BUILD_TOKENIZER:
 		tokenizer = build_tokenizer(data)
+
 
 	# Build a Dataset
 	if BUILD_DATASET:
@@ -109,53 +143,66 @@ if __name__ == '__main__':
 		print(vars(ds).keys())
 		tokenizer = ds.tokenizer
 
+
 	# Will automagically handle padding for models that require padding (Ex: Yoon Kim CNN)
 	if TRAIN_MODEL:
-		print("=== Tokenizing Dataset and Padding")
-		factory = TokenModelFactory(1, tokenizer.token_index, max_tokens=100, embedding_type='glove.6B.100d')
-		word_encoder_model = YoonKimCNN()
 
-		# Train Model
-		print("=== Training Model")
-		model = factory.build_model(token_encoder_model=word_encoder_model)
+		# Can also use `max_sents=None` to allow variable sized max_sents per mini-batch.
+		print("=== Tokenizing Dataset and Padding")
+		factory = SentenceModelFactory(NUM_CLASSES, tokenizer.token_index, max_sents=MAX_SENTS, max_tokens=MAX_TOKENS, embedding_type='glove.6B.100d')
+		word_encoder_model = AttentionRNN()
+		sentence_encoder_model = AttentionRNN()
+
+		# Allows you to compose arbitrary word encoders followed by sentence encoder.
+		print("=== Defining Model")
+		model = factory.build_model(word_encoder_model, sentence_encoder_model)
 		model.compile(optimizer='adam', loss='categorical_crossentropy')
 		model.summary()
 
+
+		# Build training data
+		X_train = ds.tokenizer.encode_texts(ds.X.tolist())
+		X_train = doc_to_sentence(X_train, MAX_SENTS, MAX_TOKENS)
+		
+		print("\tTraining data has rows = %d" % X_train.shape[0])
+		print(X_train[0])
+		print(X_train.shape)
+
+		# Convert from array to categorical
+		y_train = to_categorical(ds.y)
+
+		# Fit
+		model.fit(x=X_train, y=y_train,
+			epochs=1,
+			batch_size=512)
+
 		# Save
-		dump(factory, file_name='../data/embeddings/factory_example')
-		dump(model, file_name='../data/models/YoonKimCNN_example')
+		dump(factory, file_name=os.path.join(save_directory, 'embeddings', 'factory_sent'))
+		model.save(os.path.join(save_directory, 'models', model_name))
+
 	else:
 		print("=== Loading Embeddings")
-		factory = load(file_name='../data/embeddings/factory_example')
+		factory = load(file_name=os.path.join(save_directory, 'embeddings', 'factory_sent'))
 		print("=== Loading Model")
-		model   = load(file_name='../data/models/YoonKimCNN_example')
+		model = load_model(file_name=os.path.join(save_directory, 'models', model_name))
 
 
 	# Make predictions
 	if INFERENCE:
 		print("=== Making Inference")
-		data_infer = ['I thought the movie was terrible', 'Very fun and exciting']
+		data_infer = [
+			'I thought the movie was terrible. The way the characters talked annoyed me.', 
+			'Very fun and exciting. It was one of the best experiences I had.'
+		]
 
 		print("\t--- Tokenizing raw inference dataset")		
 		print(vars(tokenizer).keys())
-		ds_infer = tokenizer.encode_texts(data_infer)
-		ds_decode = tokenizer.decode_texts(ds_infer)
-		print(ds_infer)
-		print(ds_decode)
-		print(type(factory))
-
-		# Convert words to vector embeddings
-		ds_embedding = []
-		for i, sentence in enumerate(ds_decode):
-			print('\t\tWorking on sentence %d' % (i + 1))
-			sentence_embedding = []
-			for j, word in enumerate(sentence):
-				word_embedding = factory.embeddings_index[word.encode()]
-				if word_embedding is not None:
-					sentence_embedding.append(word_embedding)
-
-			ds_embedding.append(sentence_embedding)
+		x_test = tokenizer.encode_texts(data_infer)
+		x_test = doc_to_sentence(x_test, MAX_SENTS, MAX_TOKENS)
 
 		print("\t--- Feeding tokenized text to model")
-		pred = model.fit(x=ds_embedding)
+		pred = model.fit(x=x_test)
 		print(pred)
+		print(data_infer)
+
+
